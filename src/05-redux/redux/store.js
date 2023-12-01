c94d5635edbe205f237b100c51c7d561d312a4d1
许国栋(Guodong Xu) <gdxu@univista-isg.com>
2023-12-01 15:18:20 +0800

    createStore simple implementation

--- src/05-redux/redux/store.js
+++ src/05-redux/redux/store.js
@@ -5,7 +5,7 @@ import { createStore } from 'redux'
 
 const reducer = (prevState = {
   show: true
-}, action) => {
+}, action = {}) => {
   console.log(action)
   let newState = {...prevState}
   switch(action.type) {
@@ -21,5 +21,35 @@ const reducer = (prevState = {
 }
 
 const store = createStore(reducer)
+
+/**
+ * store.dispatch
+ * store.subscribe
+ * store.getState
+ */
+
+// function createXGDStore(reducer) {
+//   let list = []
+//   let state = reducer()
+//   function subscribe(callback) {
+//     list.push(callback)
+//   }
+
+//   function dispatch(action) {
+//     state = reducer(state, action)
+//     for(let i in list) {
+//       list[i] && list[i]()
+//     }
+//   }
+
+//   function getState() {
+//     return state
+//   }
+//   return {
+//     subscribe,
+//     dispatch,
+//     getState
+//   }
+// }
 
 export default store
