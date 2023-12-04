efd3193b3bc0ee37f4c3e5a7ea05b6c2243a7447
许国栋(Guodong Xu) <gdxu@univista-isg.com>
2023-12-04 10:59:28 +0800

    antd import

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './06-react-redux/App'
-import { Provider } from 'react-redux'
-import { store, persistor } from './06-react-redux/redux/store'
+import App from './07-antd/01-antd引入'
+// import { Provider } from 'react-redux'
+// import { store, persistor } from './06-react-redux/redux/store'
 
-import { PersistGate } from 'redux-persist/integration/react'
+// import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.render(
   // <React.StrictMode>
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <App />
-    </PersistGate>
-  </Provider>
+  // <Provider store={store}>
+  //   <PersistGate loading={null} persistor={persistor}>
+  //     <App />
+  //   </PersistGate>
+  // </Provider>
   // </React.StrictMode>
+  <App />
   ,
   document.getElementById("root"))
