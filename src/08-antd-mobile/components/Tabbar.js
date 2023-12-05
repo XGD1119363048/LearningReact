ebe0dad834bd9f924c30acad11a7e63212dc2781
许国栋(Guodong Xu) <gdxu@univista-isg.com>
2023-12-05 10:34:08 +0800

    tab & tabbar added

--- src/08-antd-mobile/components/Tabbar.js
+++ src/08-antd-mobile/components/Tabbar.js
@@ -1,21 +1,27 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
 import style from './Tabbar.module.css'
+import { TabBar } from 'antd-mobile'
+import { AppOutline, UnorderedListOutline, UserOutline } from 'antd-mobile-icons'
+import { withRouter } from 'react-router-dom'
 
-export default function Tabbar() {
+function Tabbar(props) {
+  const tabs = [
+    { key: '/films', title: '电影', icon: <AppOutline /> },
+    { key: '/cinemas', title: '影院', icon: <UnorderedListOutline /> },
+    { key: '/center', title: '我的', icon: <UserOutline /> }
+  ]
+
   return (
     <div className={style.tabbar}>
-      <ul>
-        <li>
-          <NavLink to='/films' activeClassName={style.active}>电影</NavLink>
-        </li>
-        <li>
-          <NavLink to='/cinemas' activeClassName={style.active}>影院</NavLink>
-        </li>
-        <li>
-          <NavLink to='/center' activeClassName={style.active}>我的</NavLink>
-        </li>
-      </ul>
+      <TabBar onChange={(value) => {
+        props.history.replace(value)
+      }}>
+        {tabs.map(item => (
+          <TabBar.Item key={item.key} icon={item.icon} title={item.title} />
+        ))}
+      </TabBar>
     </div>
   )
 }
+
+export default withRouter(Tabbar)
--- src/08-antd-mobile/views/Films.js
+++ src/08-antd-mobile/views/Films.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
-import { NavLink, Redirect, Route, Switch } from 'react-router-dom'
+import { Redirect, Route, Switch } from 'react-router-dom'
 
 import NowPlaying from './films/NowPlaying'
 import ComingSoon from './films/ComingSoon'
 
 import style from './css/Film.module.css'
 
-import { Swiper } from 'antd-mobile'
+import { Swiper, Tabs } from 'antd-mobile'
 import axios from 'axios'
 
 export default class Films extends Component {
@@ -35,8 +35,6 @@ export default class Films extends Component {
   render() {
     return (
       <div className={style.film + ' aaaaa'}>
-        {/* <div style={{ height: '200px', background: 'yellow' }}>大轮播</div> */}
-
         <Swiper autoplay loop>
           {
             this.state.loopList.map(item => <Swiper.Item key={item.filmId}>
@@ -48,14 +46,18 @@ export default class Films extends Component {
           }
         </Swiper>
 
-        <ul>
-          <li>
-            <NavLink to='/films/nowplaying' activeClassName={style.active}>正在热映</NavLink>
-          </li>
-          <li>
-            <NavLink to='/films/comingsoon' activeClassName={style.active}>即将上映</NavLink>
-          </li>
-        </ul>
+        <div style={{
+          position: 'sticky',
+          top: 0,
+          background: 'white'
+        }}>
+          <Tabs onChange={(value) => {
+            this.props.history.replace(value)
+          }} activeKey={this.props.location.pathname}>
+            <Tabs.Tab title='正在热映' key='/films/nowplaying' />
+            <Tabs.Tab title='即将上映' key='/films/comingsoon' />
+          </Tabs>
+        </div>
 
         {/* 路由配置 嵌套路由 */}
         <Switch>
