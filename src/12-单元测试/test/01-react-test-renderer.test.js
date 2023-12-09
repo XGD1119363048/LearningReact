2c7d0c85db733dcdb776e1a90c8ff200d155a7f3
XGD1119363048 <1119363048@qq.com>
2023-12-09 12:56:59 +0800

    ut with test-utils

--- src/12-单元测试/test/01-react-test-renderer.test.js
+++ src/12-单元测试/test/01-react-test-renderer.test.js
@@ -1,5 +1,6 @@
 import ShallowRender from 'react-test-renderer/shallow'
 import App from '../App'
+import ReactTestUtil from 'react-dom/test-utils'
 describe('react-test-renderer', () => {
   it('app 的名字是 xgd-todo', () => {
     const render = new ShallowRender()
@@ -7,5 +8,33 @@ describe('react-test-renderer', () => {
     // console.log(render.getRenderOutput().props.children[0].type)
     expect(render.getRenderOutput().props.children[0].type).toBe('h1')
     expect(render.getRenderOutput().props.children[0].props.children).toBe('xgd-todo')
+  })
+
+  it('删除功能', () => {
+    const app = ReactTestUtil.renderIntoDocument(<App/>)
+    let todoItems = ReactTestUtil.scryRenderedDOMComponentsWithTag(app, 'li')
+    console.log(todoItems.length)
+
+    let deleteButton = todoItems[0].querySelector('button')
+
+    ReactTestUtil.Simulate.click(deleteButton)
+
+    let todoItemsAfterClick = ReactTestUtil.scryRenderedDOMComponentsWithTag(app, 'li')
+
+    expect(todoItemsAfterClick.length).toBe(todoItems.length - 1)
+  })
+
+  it('添加功能', () => {
+    const app = ReactTestUtil.renderIntoDocument(<App/>)
+    let todoItems = ReactTestUtil.scryRenderedDOMComponentsWithTag(app, 'li')
+    console.log(todoItems.length)
+
+    let addInput = ReactTestUtil.scryRenderedDOMComponentsWithTag(app, 'input')
+    addInput.value = 'xgdaaaa'
+    let addButton = ReactTestUtil.findRenderedDOMComponentWithClass(app, 'add')
+    ReactTestUtil.Simulate.click(addButton)
+
+    let todoItemsAfterClick = ReactTestUtil.scryRenderedDOMComponentsWithTag(app, 'li')
+    expect(todoItemsAfterClick.length).toBe(todoItems.length + 1)
   })
 })
