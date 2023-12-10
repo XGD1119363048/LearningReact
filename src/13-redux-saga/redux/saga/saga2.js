2d990dc7bdb79e774c122649dbe03b374b2b7eff
XGD1119363048 <1119363048@qq.com>
2023-12-10 18:24:37 +0800

    saga api先后调用

--- src/13-redux-saga/redux/saga/saga2.js
+++ src/13-redux-saga/redux/saga/saga2.js
@@ -13,19 +13,28 @@ function *getList2() {
   // 异步处理
 
   // call 函数发异步请求
-  let res = yield call(getListAction2) // 阻塞的调用 fn
+  let res1 = yield call(getListAction2_1) // 阻塞的调用 fn
+  let res2 = yield call(getListAction2_2, res1) // 阻塞的调用 fn
 
   yield put({
     type: 'change-list2',
-    payload: res
+    payload: res2
   })
   // put 函数发出新的 action，非阻塞式执行（立即执行）
 }
 
-function getListAction2() {
+function getListAction2_1() {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       resolve(['444', '555', '666'])
+    }, 2000)
+  })
+}
+
+function getListAction2_2(data) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve([...data, '777', '888', '999'])
     }, 2000)
   })
 }
